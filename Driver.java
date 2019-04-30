a8f6f32f9c98643adade333ff0b551ccb6e90be9
Hilary Zen <hzen00@stuy.edu>
2019-04-30 02:07:22 -0400

    Edited and tested

--- Driver.java
+++ Driver.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class Driver {
   public static void main(String[] args) {
     int[] array = {3, 5, 2, 9, 14, 1};
+    MyHeap.pushDown(array, 6, 0);
+    System.out.println(Arrays.toString(array));
   }
 }
--- MyHeap.java
+++ MyHeap.java
@@ -1,12 +1,12 @@
 public class MyHeap {
   //size is the number of elements in the array
   //pushes element at index downward into the right position
-  private static void pushDown(int[] data, int size, int index) {
+  public static void pushDown(int[] data, int size, int index) {
     //Checks if index is within bounds and one of its children is larger
-    while (index > size && (data[index] < data[index * 2] || data[index] < data[index * 2 + 1]) {
-      if (data[index * 2] > data[index * 2 + 1]) { //Left node is larger
+    while (index < size && (data[index] < data[(index + 1) * 2 - 1] || data[index] < data[(index + 1) * 2])) {
+      if (data[(index + 1) * 2 - 1] > data[(index + 1) * 2]) { //Left node is larger
         int value = data[index];
-        int newI = index * 2;
+        int newI = (index + 1) * 2 - 1;
         //Swaps the two nodes
         data[index] = data[newI];
         data[newI] = value;
@@ -14,7 +14,7 @@ public class MyHeap {
         index = newI;
       } else { //Right node is larger
         int value = data[index];
-        int newI = index * 2 + 1;
+        int newI = (index + 1) * 2;
         //Swaps the two nodes
         data[index] = data[newI];
         data[newI] = value;
