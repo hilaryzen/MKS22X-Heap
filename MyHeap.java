0ab4044bc5d375111511be1bf76f52540ce97f5a
Hilary Zen <hzen00@stuy.edu>
2019-04-30 02:09:26 -0400

    Fixed pushDown

--- MyHeap.java
+++ MyHeap.java
@@ -3,7 +3,7 @@ public class MyHeap {
   //pushes element at index downward into the right position
   public static void pushDown(int[] data, int size, int index) {
     //Checks if index is within bounds and one of its children is larger
-    while (index < size && (data[index] < data[(index + 1) * 2 - 1] || data[index] < data[(index + 1) * 2])) {
+    while ((index + 1) * 2 < size && (data[index] < data[(index + 1) * 2 - 1] || data[index] < data[(index + 1) * 2])) {
       if (data[(index + 1) * 2 - 1] > data[(index + 1) * 2]) { //Left node is larger
         int value = data[index];
         int newI = (index + 1) * 2 - 1;
