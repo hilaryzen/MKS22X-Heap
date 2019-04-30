3233deacc413ceb9d3a702f156559d5b1dc6a5ed
Hilary Zen <hzen00@stuy.edu>
2019-04-30 10:55:20 -0400

    Fixed pushUp

--- Driver.java
+++ Driver.java
@@ -3,9 +3,17 @@ import java.util.Arrays;
 public class Driver {
   public static void main(String[] args) {
     int[] array = {3, 5, 2, 9, 14, 1};
+
+    System.out.println("Testing pushDown");
     MyHeap.pushDown(array, 6, 0);
     System.out.println(Arrays.toString(array));
     MyHeap.pushDown(array, 6, 0);
     System.out.println(Arrays.toString(array));
+
+    int[] array2 = {3, 6, 2, 9, 14, 1};
+    System.out.println(" ");
+    System.out.println("Testing pushUp");
+    MyHeap.pushUp(array2, 4);
+    System.out.println(Arrays.toString(array2));
   }
 }
--- MyHeap.java
+++ MyHeap.java
@@ -26,7 +26,7 @@ public class MyHeap {
 
   public static void pushUp(int[] data, int index) {
     //If index has a parent smaller than it
-    while ((index - 1) / 2 > 0 && (data[(index - 1) / 2] < data[index])) {
+    while ((index - 1) / 2 >= 0 && (data[(index - 1) / 2] < data[index])) {
       int value = data[index];
       int newI = (index - 1) / 2;
       //Swaps the two nodes
@@ -39,8 +39,8 @@ public class MyHeap {
 
   //Turns an array into a heap by working backwards through the nodes and pushing them down
   public static void heapify(int[] data) {
-    for (int i = data.size - 1; i >= 0; i--) {
-      pushDown(data, data.size, i);
+    for (int i = data.length - 1; i >= 0; i--) {
+      pushDown(data, data.length, i);
     }
   }
 }
