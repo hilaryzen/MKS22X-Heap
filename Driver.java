41d1a97a8514ae2c179488447e8ffdce1e17c808
Hilary Zen <hzen00@stuy.edu>
2019-04-30 01:59:10 -0400

    Edited pushDown

---
+++ Driver.java
@@ -0,0 +1,5 @@
+public class Driver {
+  public static void main(String[] args) {
+    int[] array = {3, 5, 2, 9, 14, 1};
+  }
+}
--- MyHeap.java
+++ MyHeap.java
@@ -2,14 +2,24 @@ public class MyHeap {
   //size is the number of elements in the array
   //pushes element at index downward into the right position
   private static void pushDown(int[] data, int size, int index) {
-    int i = data[index];
-    while (i < data[index * 2] || i < data[index * 2 + 1]) {
-      if (data[index * 2] > data[index * 2 + 1]) {
-        data[index] = data[index * 2];
-        data[index * 2] = i;
-      } else {
-        data[index] = data[index * 2 + 1];
-        data[index * 2 + 1] = i;
+    //Checks if index is within bounds and one of its children is larger
+    while (index > size && (data[index] < data[index * 2] || data[index] < data[index * 2 + 1]) {
+      if (data[index * 2] > data[index * 2 + 1]) { //Left node is larger
+        int value = data[index];
+        int newI = index * 2;
+        //Swaps the two nodes
+        data[index] = data[newI];
+        data[newI] = value;
+        //Index changes to new position
+        index = newI;
+      } else { //Right node is larger
+        int value = data[index];
+        int newI = index * 2 + 1;
+        //Swaps the two nodes
+        data[index] = data[newI];
+        data[newI] = value;
+        //Index changes to new position
+        index = newI;
       }
     }
   }
