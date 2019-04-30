aabc35ce785343c242a3fb1aa32701f59776dca3
Hilary Zen <hzen00@stuy.edu>
2019-04-30 02:10:18 -0400

    Added test

--- Driver.java
+++ Driver.java
@@ -5,5 +5,7 @@ public class Driver {
     int[] array = {3, 5, 2, 9, 14, 1};
     MyHeap.pushDown(array, 6, 0);
     System.out.println(Arrays.toString(array));
+    MyHeap.pushDown(array, 6, 0);
+    System.out.println(Arrays.toString(array));
   }
 }
